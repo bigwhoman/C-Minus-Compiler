ead9fc3f8dc579350dae493d98bc4a63a3c87c25
Hirbod Behnam <hirbod.behnam.81@gmail.com>
2024-04-04 13:39:01 +0330

    Fixed some bugs

--- compiler.py
+++ compiler.py
@@ -20,9 +20,10 @@ class Scanner():
         TokenType.NUM: list(string.digits),
         TokenType.KEYWORD: ["if", "else", "void", "int", "for",
                     "break", "return", "endif"],
-        TokenType.SYMBOL: [';', ':', '[', ']', '(', ')', '{', '}', '+',
+        TokenType.SYMBOL: [';', ':', ',', '[', ']', '(', ')', '{', '}', '+',
                    '-', '*', '=', '<'],
         TokenType.WHITESPACE: ['\n', '\r', '\t', '\v', '\f', ' '],
+        TokenType.COMMENT: ['/'],
         TokenType.LETTER: list(string.ascii_letters)
     }
 
@@ -82,7 +83,7 @@ class Scanner():
                                                      self.pointer_end], "Invalid input"))
 
     def ID_DFA(self) -> str:
-        others = self.TOKEN_TYPES[TokenType.SYMBOL] + self.TOKEN_TYPES[TokenType.WHITESPACE]
+        others = self.TOKEN_TYPES[TokenType.SYMBOL] + self.TOKEN_TYPES[TokenType.WHITESPACE] + self.TOKEN_TYPES[TokenType.COMMENT]
         while self.pointer_end < len(self.current_line):
             if self.current_line[self.pointer_end] in self.TOKEN_TYPES[TokenType.LETTER] or \
                     self.current_line[self.pointer_end] in self.TOKEN_TYPES[TokenType.NUM]:
@@ -91,13 +92,14 @@ class Scanner():
                 break
             else:
                 self.pointer_end += 1
-                raise SyntaxError((self.current_line[self.pointer_start], "Invalid input"))
+                raise SyntaxError((self.current_line[self.pointer_start:
+                                 self.pointer_end], "Invalid input"))
 
         return self.current_line[self.pointer_start:
                                  self.pointer_end]
 
     def NUM_DFA(self) -> str:
-        others = self.TOKEN_TYPES[TokenType.SYMBOL] + self.TOKEN_TYPES[TokenType.WHITESPACE]
+        others = self.TOKEN_TYPES[TokenType.SYMBOL] + self.TOKEN_TYPES[TokenType.WHITESPACE] + self.TOKEN_TYPES[TokenType.COMMENT]
         while self.pointer_end < len(self.current_line):
             if self.current_line[self.pointer_end] in self.TOKEN_TYPES[TokenType.NUM]:
                 self.pointer_end += 1
@@ -115,16 +117,21 @@ class Scanner():
         pass
 
     def SYMBOL_DFA(self) -> str:
-        if self.current_line[self.pointer_start] == "=" :
-            if len(self.current_line) != self.pointer_start + 1 and self.current_line[self.pointer_start + 1] == "=" : 
-                self.pointer_end += 2
-                return "=="
-            else:
-                self.pointer_end += 1
-                return "="
-        elif len(self.current_line) != self.pointer_start + 1 and self.current_line[self.pointer_start:self.pointer_start + 1] == "*/":
+        other = self.TOKEN_TYPES[TokenType.NUM] + self.TOKEN_TYPES[TokenType.LETTER] + self.TOKEN_TYPES[TokenType.SYMBOL] + self.TOKEN_TYPES[TokenType.COMMENT] + self.TOKEN_TYPES[TokenType.WHITESPACE]
+        # Check end of the line early
+        if len(self.current_line) == self.pointer_start + 1:
+            self.pointer_end += 1
+            return self.current_line[self.pointer_start]
+        # We are not at the end of the line
+        if self.current_line[self.pointer_start:self.pointer_start + 2] == "==":
+            self.pointer_end += 2
+            return "=="
+        elif self.current_line[self.pointer_start:self.pointer_start + 2] == "*/":
             self.pointer_end += 2
             raise SyntaxError(("*/", "Unmatched comment"))
+        elif not self.current_line[self.pointer_start + 1] in other:
+            self.pointer_end += 2
+            raise SyntaxError((self.current_line[self.pointer_start:self.pointer_start + 2], "Invalid input"))
         else:
             self.pointer_end += 1
             return self.current_line[self.pointer_start]
@@ -167,6 +174,8 @@ def main():
             token = scanner.get_next_token()
             if token == None:
                 break
+            if token[0] == TokenType.WHITESPACE or token[0] == TokenType.COMMENT:
+                    continue # skip whitespace and comment
             if not scanner.line_number in tokens:
                 tokens[scanner.line_number] = []
             tokens[scanner.line_number].append(token)
@@ -179,9 +188,7 @@ def main():
         for line_number, token_list in sorted(tokens.items()):
             tokens_file.write(f"{line_number}.\t")
             for token in token_list:
-                if token[0] == TokenType.WHITESPACE or token[0] == TokenType.COMMENT:
-                    continue # skip whitespace and comment
-                tokens_file.write(f"({token[0].name} {token[1]}) ")
+                tokens_file.write(f"({token[0].name}, {token[1]}) ")
             tokens_file.write("\n")
     with open("lexical_errors.txt", "w") as lexical_errors_file:
         if len(errors) == 0:
@@ -190,7 +197,7 @@ def main():
             for line_number, errors_list in sorted(errors.items()):
                 lexical_errors_file.write(f"{line_number}.\t")
                 for error in errors_list:
-                    lexical_errors_file.write(f"({error[0]} {error[1]}) ")
+                    lexical_errors_file.write(f"({error[0]}, {error[1]}) ")
                 lexical_errors_file.write("\n")
     with open("symbol_table.txt", "w") as symbol_table_file:
         for index, symbol in sorted(scanner.symbol_table.items()):
